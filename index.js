cb039d7a8ae2e0e6782a7a4a8278e0c07210e8e4
Emma <ramirezerick15@gmail.com>
2015-12-06 16:32:03 -0500

    add faith purchases

--- index.js
+++ index.js
@@ -954,146 +954,194 @@ var game = {
         name: "Goddess of the Halibut",
         level: "Pantheon",
         costMult: 1,
-        effect: "Converts half your <img src='img/fish.png' /> into <img src='img/faith.png' />PM."
+        effect: "Converts half your <img src='img/fish.png' /> into <img src='img/faith.png' />PM.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Spirit of the Spices",
         level: "Pantheon",
         costMult: 1,
-        effect: "+1% <img src='img/faith.png' /> per <img src='img/spices.png' /> (max 25%)."
+        effect: "+1% <img src='img/faith.png' /> per <img src='img/spices.png' /> (max 25%).",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Benevelont Muses",
         level: "Pantheon",
         costMult: 1,
-        effect: "+.5 <img src='img/faith.png' /> PM for each Artist you have."
+        effect: "+.5 <img src='img/faith.png' /> PM for each Artist you have.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "God of War",
         level: "Pantheon",
         costMult: 1,
-        effect: "+15% <img src='img/strength.png' />."
+        effect: "+15% <img src='img/strength.png' />.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Satyr's Charm",
         level: "Pantheon",
         costMult: 1,
-        effect: "+1% <img src='img/happy.png' /> per <img src='img/horse.png' /> (max 25%)."
+        effect: "+1% <img src='img/happy.png' /> per <img src='img/horse.png' /> (max 25%).",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Golden Calf",
         level: "Pantheon",
         costMult: 1,
-        effect: "+1 <img src='img/faith.png' /> PM per <img src='img/gold.png' /> resource."
+        effect: "+1 <img src='img/faith.png' /> PM per <img src='img/gold.png' /> resource.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Fertility Goddess",
         level: "Pantheon",
         costMult: 1,
-        effect: "+10 <img src='img/food.png' /> PC."
+        effect: "+10 <img src='img/food.png' /> PC.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Protective Spirits",
         level: "Pantheon",
         costMult: 1,
-        effect: "+30% <img src='img/defense.png' />."
+        effect: "+30% <img src='img/defense.png' />.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Stone Circles",
         level: "Pantheon",
         costMult: 1,
-        effect: "+.2 <img src='img/faith.png' /> PM per Quarry."
+        effect: "+.2 <img src='img/faith.png' /> PM per Quarry.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Lord of the Flies",
         level: "Pantheon",
         costMult: 1,
-        effect: "+1 <img src='img/happy.png' /> per 4 <img src='img/pollution.png' />"
+        effect: "+1 <img src='img/happy.png' /> per 4 <img src='img/pollution.png' />",
+        unlocked: false,
+        activated: false
       },
       {
         name: "The Furies",
         level: "Pantheon",
         costMult: 1,
         effect: "+10 <img src='img/faith.png'/> when you declare war.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "The Fates",
         level: "Pantheon",
         costMult: 1,
-        effect: "Increases chance of good events."
+        effect: "Increases chance of good events.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Doggy Cult",
         level: "Pantheon",
         costMult: 1,
-        effect: "Converts all <img src='img/horse.png' /> into Chihuahuas."
+        effect: "Converts all <img src='img/horse.png' /> into Chihuahuas.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Spaghetti Monster",
         level: "Pantheon",
         costMult: 1,
-        effect: "+1 Spaghetti resource."
+        effect: "+1 Spaghetti resource.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Monotheism",
         level: "Belief",
         costMult: 25,
-        effect: "+3 <img src='img/faith.png' /> PM per Cleric."
+        effect: "+3 <img src='img/faith.png' /> PM per Cleric.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Reincarnation",
         level: "Belief",
         costMult: 25,
-        effect: "+15 <img src='img/faith.png' /> PM per Restart."
+        effect: "+15 <img src='img/faith.png' /> PM per Restart.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Nirvana",
         level: "Belief",
         costMult: 25,
-        effect: "25% of <img src='img/faith.png' /> PM converted into <img src='img/happy.png' />"
+        effect: "25% of <img src='img/faith.png' /> PM converted into <img src='img/happy.png' />",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Polytheism",
         level: "Belief",
         costMult: 25,
-        effect: "+.1 <img src='img/faith.png' /> from all Citizens."
+        effect: "+.1 <img src='img/faith.png' /> from all Citizens.",
+        unlocked: false,
+        activated: false
       },
 
       {
         name: "Purgatory",
         level: "Dogma",
         costMult: 125,
-        effect: "+25 <img src='img/faith.png' /> amidst indecisive wars."
+        effect: "+25 <img src='img/faith.png' /> amidst indecisive wars.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Pilgrimage",
         level: "Dogma",
         costMult: 125,
-        effect: "+5 Free Citizens"
+        effect: "+5 Free Citizens",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Virgin Birth",
         level: "Dogma",
         costMult: 125,
-        effect: "Retain 80% of <img src='img/food.png' /> after growth."
+        effect: "Retain 80% of <img src='img/food.png' /> after growth.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Ressurection",
         level: "Dogma",
         costMult: 125,
-        effect: "Retain Max <img src='img/food.png' /> after Reset."
+        effect: "Retain Max <img src='img/food.png' /> after Reset.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Avatar",
         level: "Dogma",
         costMult: 125,
-        effect: "+100 <img src='img/strength.png' /> per Reset."
+        effect: "+100 <img src='img/strength.png' /> per Reset.",
+        unlocked: false,
+        activated: false
       },
       {
         name: "Just War",
         level: "Dogma",
         costMult: 125,
-        effect: "+25% <img src='img/strength.png'/> when fighting a war you did not declare."
+        effect: "+25% <img src='img/strength.png'/> when fighting a war you did not declare.",
+        unlocked: false,
+        activated: false
       }
     ]
   },
@@ -3070,17 +3118,43 @@ var game = {
 
     var setFaithUpgrades = function() {
       var section = $(".faith-upgrades");
+      $(".faith-upgrades [data-button]").remove();
       var cost = 10;
 
       for (var i = 0; i < game.faith.upgrades.length; i++) {
+        if (game.faith.upgrades[i].unlocked) {
+          $(".faith-upgrades." + game.faith.upgrades[i].level).append("<div data-button='faith-upgrade' data-n='" + i + "' class='unlocked'><img src='img/faith.png' /> <strong>" + (cost * game.faith.upgrades[i].costMult) + "</strong> " + game.faith.upgrades[i].name + "&nbsp;&nbsp;&nbsp;<em class='faith-upgrade-effect'>" + game.faith.upgrades[i].effect + "</em></div>");
+        } else {
+          $(".faith-upgrades." + game.faith.upgrades[i].level).append("<div data-button='faith-upgrade' data-n='" + i + "'><img src='img/faith.png' /> <strong>" + (cost * game.faith.upgrades[i].costMult) + "</strong> " + game.faith.upgrades[i].name + "&nbsp;&nbsp;&nbsp;<em class='faith-upgrade-effect'>" + game.faith.upgrades[i].effect + "</em></div>");
+        }
 
-        $(".faith-upgrades." + game.faith.upgrades[i].level).append("<div data-button='faith-upgrade'><img src='img/faith.png' /> <strong>" + (cost * game.faith.upgrades[i].costMult) + "</strong> " + game.faith.upgrades[i].name + "&nbsp;&nbsp;&nbsp;<em class='faith-upgrade-effect'>" + game.faith.upgrades[i].effect + "</em></div>");
       }
+
+      $("[data-button='faith-upgrade']").click(function(){
+        var n = $(this).attr('data-n');
+        if (game.faith.total >= (game.faith.upgrades[n].costMult * 10)) {
+
+          game.faith.total -= (game.faith.upgrades[n].costMult * 10);
+          game.faith.upgrades[n].unlocked = true;
+          setFaithUpgrades();
+          checkFaithUpgrades(n);
+        } else {
+          note("You don't have enough Faith to purchase this upgrade!");
+        }
+      });
+
     };
     setFaithUpgrades();
 
-    var checkFaithUpgrades = function() {
+    var checkFaithUpgrades = function(n) {
+      if (!game.culture.upgrades[n].activated) {
+        if (n == 0) {
 
+        }
+        game.culture.upgrades[n].activated = true;
+      } else {
+        note("You already activated that culture purchase!");
+      }
     };
     checkFaithUpgrades();
 
