bf5e966b9a22977e6806f1bf8997c196ce2aaab9
Emma <ramirezerick15@gmail.com>
2015-12-13 00:53:13 -0500

    implementation of localstroage get/set prelim

--- clickopolis-beta/js/index.js
+++ clickopolis-beta/js/index.js
@@ -6,15 +6,46 @@ var data = {
       ps: 45,
       pc: 0,
       max: 0
-    }
+    },
+    prod: {
+      total: 10,
+      ps: 0,
+      pc: 0,
+      max: 0
+    },
   }
 
 };
 
 var game = {
   init: function() {
-    //game.resources.food.total = localStorage.getItem('foodTotal');
 
+    game.getData();
+    game.updateData();
+
+    $("[data-post]").click(function(){
+      data.resources.food.total += 1;
+      data.resources.food.pc += 100;
+
+      game.updateData();
+      game.setData();
+    });
+
+  },
+
+  getData: function() {
+    data.resources.food.total   = Lockr.get('foodTotal') || data.resources.food.total;
+    data.resources.food.ps      = Lockr.get('foodPS') || data.resources.food.ps;
+    data.resources.food.pc      = Lockr.get('foodPC') || data.resources.food.pc;
+  },
+
+  setData: function() {
+    Lockr.set('foodTotal', data.resources.food.total);
+    Lockr.set('foodPS', data.resources.food.ps);
+    Lockr.set('foodPC', data.resources.food.pc);
+  },
+
+  updateData: function() {
     $("[data-post]").text(function(){
       var d = $(this).attr('data-post');
       d = d.split('.');
@@ -23,12 +54,6 @@ var game = {
       if (d.length == 3) return data[d[0]][d[1]][d[2]];
       if (d.length == 4) return data[d[0]][d[1]][d[2]][d[3]];
     });
-
-    $("[data-post]").click(function(){
-      //data.resources.food.total += 1;
-      //localStorage.setItem('foodtotal', data.resources.food.total);
-    });
-
   }
 
 
