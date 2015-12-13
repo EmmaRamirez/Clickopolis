84eb44378c31acb7fd874208876a45cfab7d832e
Emma <ramirezerick15@gmail.com>
2015-12-13 01:26:42 -0500

    add achievemtns

--- clickopolis-beta/js/gulpfile.js
+++ clickopolis-beta/js/gulpfile.js
@@ -1,26 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var handlebars = require('gulp-handlebars');
-var wrap = require('gulp-wrap');
-var declare = require('gulp-declare');
-var concat = require('gulp-concat');
 
-gulp.task('default', function() {
-  console.log("hi");
-});
-
-gulp.task('templates', function(){
-  gulp.src('source/templates/*.hbs')
-    .pipe(handlebars())
-    .pipe(wrap('Handlebars.template(<%= contents %>)'))
-    .pipe(declare({
-      namespace: 'MyApp.templates',
-      noRedeclare: true, // Avoid duplicate declarations
-    }))
-    .pipe(concat('templates.js'))
-    .pipe(gulp.dest('build/js/'));
-});
 
 gulp.task('styles', function() {
   gulp.src('sass/**/*.scss')
