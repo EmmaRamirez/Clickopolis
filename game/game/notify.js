a1e0f13814988fcf6b4c29e530dc249a75112df9
Emma <ramirez.emma.g@gmail.com>
2016-05-29 13:22:35 -0400

    add color option to notify

--- game/game/notify.js
+++ game/game/notify.js
@@ -1,9 +1,11 @@
 "use strict";
-function notify(message) {
+function notify(message, color) {
+    if (color === void 0) { color = '#222'; }
     // TODO: create settimeout
     var note = document.createElement('div');
     note.className = 'notification';
     note.textContent = message;
+    note.style.backgroundColor = color;
     document.body.appendChild(note);
     setTimeout(function () {
         note.className = 'notification hidden';
--- game/game/notify.ts
+++ game/game/notify.ts
@@ -1,9 +1,10 @@
-function notify(message:string):void {
+function notify(message:string, color:string = '#222'):void {
   // TODO: create settimeout
   let note = document.createElement('div');
   note.className = 'notification';
 
   note.textContent = message;
+  note.style.backgroundColor = color;
   document.body.appendChild(note);
   setTimeout(function () {
     note.className = 'notification hidden';
