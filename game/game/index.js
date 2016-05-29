69c660c1c8c3743399be7bc55205b1a0c23b1436
Emma <ramirez.emma.g@gmail.com>
2016-05-29 10:55:01 -0400

    add more techs

--- game/game/index.js
+++ game/game/index.js
@@ -40,6 +40,10 @@ var herbalMedicine = new Tech('herbal medicine', 'ancient', '', ['Can build Asce
 var masonry = new Tech('masonry', 'ancient', 'wububuu', ['', '']);
 var mining = new Tech('mining', 'ancient', 'not safe for minors', ['+.2 <img src="img/prod.png"> PS per miner', 'Unlocks: Masonry, Pottery']);
 var mysticism = new Tech('mysticism', 'ancient', 'Mysterious gods bring riches, temples, and a couple blood sacrifices.', ['Can assign Clerics.', 'Can build Temples.', 'Can build Stonehenge.']);
+var sailing = new Tech('sailing', 'ancient', 'It\'s a lot harder to sail if you stay at half-mast!', ['Can assign soldiers as Navy.', 'Can meet Coastal and Oceanic Nations.']);
+var trading = new Tech('trading', 'ancient', 'My six chickens for your goat?', ['Unlocks Bartering Economic System.', 'Can assign Merchants.']);
+var woodworking = new Tech('woodworking', 'ancient', 'TIMBER!!!', ['Unlocks <img src="img/spices.png"> resources.', 'Can assign Woodcutters.']);
+var writing = new Tech('writing', 'ancient', 'Allows poorly written fanfiction in Information era.', ['Unlocks Diplomacy.', 'Can build Library.']);
 var techs = new Techs([agriculture,
     animalHusbandry,
     archery,
@@ -47,7 +51,11 @@ var techs = new Techs([agriculture,
     herbalMedicine,
     masonry,
     mining,
-    mysticism]);
+    mysticism,
+    sailing,
+    trading,
+    woodworking,
+    writing]);
 var farmer = new Citizen('farmer', 'farmer', 0, 'a farmer', 1, 0);
 var citizens = new Citizens([farmer]);
 var u = new Utils();
--- game/game/index.ts
+++ game/game/index.ts
@@ -48,6 +48,10 @@ let herbalMedicine:Tech = new Tech('herbal medicine', 'ancient', '', ['Can build
 let masonry:Tech = new Tech('masonry', 'ancient', 'wububuu', ['', '']);
 let mining:Tech = new Tech('mining', 'ancient', 'not safe for minors', ['+.2 <img src="img/prod.png"> PS per miner', 'Unlocks: Masonry, Pottery']);
 let mysticism:Tech = new Tech('mysticism', 'ancient', 'Mysterious gods bring riches, temples, and a couple blood sacrifices.', ['Can assign Clerics.', 'Can build Temples.', 'Can build Stonehenge.']);
+let sailing:Tech = new Tech('sailing', 'ancient', 'It\'s a lot harder to sail if you stay at half-mast!', ['Can assign soldiers as Navy.', 'Can meet Coastal and Oceanic Nations.']);
+let trading:Tech = new Tech('trading', 'ancient', 'My six chickens for your goat?', ['Unlocks Bartering Economic System.', 'Can assign Merchants.']);
+let woodworking:Tech = new Tech('woodworking', 'ancient', 'TIMBER!!!', ['Unlocks <img src="img/spices.png"> resources.', 'Can assign Woodcutters.']);
+let writing:Tech = new Tech('writing', 'ancient', 'Allows poorly written fanfiction in Information era.', ['Unlocks Diplomacy.', 'Can build Library.']);
 
 
 let techs:Techs = new Techs([agriculture,
@@ -57,7 +61,11 @@ let techs:Techs = new Techs([agriculture,
                             herbalMedicine,
                             masonry,
                             mining,
-                            mysticism]);
+                            mysticism,
+                            sailing,
+                            trading,
+                            woodworking,
+                            writing]);
 
 let farmer:Citizen = new Citizen('farmer', 'farmer', 0, 'a farmer', 1, 0);
 
