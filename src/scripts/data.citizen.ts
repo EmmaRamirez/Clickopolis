45a978e62437e95eaf97cb4e07880d0c43313206
emmaramirez <ramirez.emma.g@gmail.com>
2016-06-23 12:22:25 -0700

    delete Citizens Class

--- src/scripts/data.citizen.ts
+++ src/scripts/data.citizen.ts
@@ -1,10 +1,10 @@
 import Citizen = require('./citizen');
 import Collection = require('./collection');
 
-let farmer:Citizen = new Citizen('farmer', 'farmer', 0, 'a farmer', 1, 0);
-let miner:Citizen = new Citizen('miner', 'miner', 0, 'a miner', 1, 0);
-let soldier:Citizen = new Citizen('soldier', 'soldier', 0, 'a soldier', 1, 0);
-let cleric:Citizen = new Citizen('cleric', 'cleric', 0, 'a cleric', 1, 0);
+let farmer:Citizen = new Citizen('farmer', 'farmer', 0, 'Farmers provide +1 <img src="img/food.png"> PC and +.2 PC.', 1.2, 0);
+let miner:Citizen = new Citizen('miner', 'miner', 0, 'Miners provide +1 <img src="img/prod.png"> PC and +.2 PC.', 0, 0);
+let soldier:Citizen = new Citizen('soldier', 'soldier-alt', 0, 'Soldiers defend and fight for your empire. -3 <img src="img/coin.png">', 0, 0);
+let cleric:Citizen = new Citizen('cleric', 'cleric', 0, 'Clerics heal your empire. +1 <img src="img/health.png">, +1 <img src="img/faith.png"> PM', 0, 0);
 
 let citizens:Collection = new Collection('Citizens', [farmer, miner, soldier, cleric]);
 
