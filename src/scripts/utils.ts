bfcd21777dbfb7c3e060a738c092846bcd0e3226
emmaramirez <ramirez.emma.g@gmail.com>
2016-06-23 14:42:19 -0700

    add: time utility

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -55,6 +55,13 @@ class Utils {
     return randomColor;
   }
 
+  time(d:number):string {
+    let h = Math.floor(d / 3600);
+    let m = Math.floor(d % 3600 / 60);
+    let s = Math.floor(d % 3600 % 60);
+    return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s);
+  }
+
 }
 
 export = Utils;
