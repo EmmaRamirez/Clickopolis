b815a757671d69516c1601570db91f1404bc6deb
emmaramirez <ramirez.emma.g@gmail.com>
2016-06-22 18:23:46 -0700

    add citizen data

---
+++ game/src/scripts/data.citizen.ts
@@ -0,0 +1,10 @@
+import Citizen = require('./citizen');
+import Collection = require('./collection');
+
+let farmer:Citizen = new Citizen('farmer', 'farmer', 0, 'a farmer', 1, 0);
+let miner:Citizen = new Citizen('miner', 'miner', 0, 'a miner', 1, 0);
+let soldier:Citizen = new Citizen('soldier', 'soldier', 0, 'a soldier', 1, 0);
+
+let citizens:Collection = new Collection('Citizens', [farmer, miner, soldier]);
+
+export = citizens;
