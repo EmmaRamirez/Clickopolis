b9e1abde416e02bd179bb99f15ae4a724e8f1db7
emmaramirez <ramirez.emma.g@gmail.com>
2016-06-24 11:34:40 -0700

    fix: notify function
    Change textContent -> innerHTML (more rich)
    Add styling option
    Remove .remove()
    Add console.log() for error checking

--- src/scripts/notify.ts
+++ src/scripts/notify.ts
@@ -1,15 +1,18 @@
-function notify(message:string, color:string = '#222', time:number = 2500):void {
+function notify(message:string, color:string = '#222', time:number = 2500, css:string = ''):void {
   // TODO: create settimeout
+  console.debug('Note was created with message of: ' + message);
   let note = document.createElement('div');
   note.className = 'notification';
 
-  note.textContent = message;
+  note.innerHTML = message;
   note.style.backgroundColor = color;
+  note.setAttribute('style', css);
   document.body.appendChild(note);
   setTimeout(function () {
     note.className = 'notification hidden';
   }, time);
-  note.remove();
+
+  console.log(note);
 }
 
 export = notify;
