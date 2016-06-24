cc49fd88489dcd71993d78cecde9d862d55de703
emmaramirez <ramirez.emma.g@gmail.com>
2016-06-24 14:47:40 -0700

    change: base Resource maximum to 100 & 500

--- src/scripts/data.resource.ts
+++ src/scripts/data.resource.ts
@@ -2,7 +2,7 @@ import Resource = require('./resource');
 import Collection = require('./collection');
 
 let food:Resource = new Resource('food', 1, 0, 1000, 0, 'food', 'Food.');
-let prod:Resource = new Resource('prod', 1, 0, 2000, 0, 'prod', 'Prod.');
+let prod:Resource = new Resource('prod', 1, 0, 500, 0, 'prod', 'Prod.');
 let stone:Resource = new Resource('stone', 0, 0, -1, 0, 'stone', 'Stones are important as a building block for buildings.');
 let fish:Resource = new Resource('fish', 0, 0, -1, 0, 'fish', 'Fish are caught in nets by citizens periodically. Each fish provides +.5 <img src="img/health.png"> Fish are a popular trade item with Desert nations.');
 let banana:Resource = new Resource('banana', 0, 0, -1, 0, 'banana', 'Bananas are harvested by farmers periodically. Each banana provides +.5 <img src="img/health.png"> Bananas are a popular trade item with Tundra nations.');
