0eadddddfe46c6f95a9612365b7f9d3f0380df75
emmaramirez <ramirez.emma.g@gmail.com>
2016-06-23 20:08:24 -0700

    add: 3 new Ancient buildings
    quarry, graveyard, and fort!

--- src/scripts/data.building.ts
+++ src/scripts/data.building.ts
@@ -3,13 +3,16 @@ import Collection = require('./collection');
 
 let hut:Building = new Building('Hut', 0, 15, 150, 'Air conditioning would be nice though.', '+1 <img src="img/happy.png">');
 let granary:Building = new Building('Granary', 0, 25, 250, 'Feeds all your cute animals.', '+200 <img src="img/food.png"> max');
+let quarry:Building = new Building('Quarry', 0, 30, 300, 'Your very own diamond in the rough.', '+200 <img src="img/prod.png"> max');
 let barracks:Building = new Building('Barracks', 0, 35, 350, 'Keep your soldiers at the ready!', '+1 <img src="img/strength.png">');
 let temple:Building = new Building('Temple', 0, 45, 450, 'Cultivate your empire\'s faith.', '+1 <img src="img/faith.png"> PM');
 let asclepeia:Building = new Building('Asclepeia', 0, 50, 500, 'We can\'t pronounce it either.', '+1 <img src="img/health.png">')
+let graveyard:Building = new Building('Graveyard', 0, 70, 700, 'Here, your (dead) citizens gather.', '-1 <img src="img/pollution.png">');
+let fort:Building = new Building('Fort', 0, 100, 1000, 'Defend the nation! Defend the state!', '+5 <img src="img/defense.png">');
 
 let buildings:Collection = new Collection('Buildings',
                                           [
-                                            hut, granary, barracks, temple, asclepeia
+                                            hut, granary, quarry, barracks, temple, asclepeia, graveyard, fort
                                           ])
 
 // name:string, amount:number, prodCost:number, cashCost:number, description:string, effect:string
