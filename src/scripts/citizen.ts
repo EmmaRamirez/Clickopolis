555bf7b5c8e1acd2732dc8f4985beb752e09a4e6
emmaramirez <ramirez.emma.g@gmail.com>
2016-06-24 17:56:09 -0700

    add function to citizen

--- src/scripts/citizen.ts
+++ src/scripts/citizen.ts
@@ -14,13 +14,16 @@ class Citizen {
   scienceContribution: number;
   influenceContribution: number;
 
-  constructor(name:string, image:string, amount: number, description: string, foodContribution: number, prodContribution: number) {
+  func: Function;
+
+  constructor(name:string, image:string, amount: number, description: string, foodContribution: number, prodContribution: number, func:Function = function(){}) {
     this.name = name;
     this.image = image;
     this.amount = amount;
     this.description = description;
     this.foodContribution = foodContribution;
     this.prodContribution = prodContribution;
+    this.func = func;
   }
 }
 
