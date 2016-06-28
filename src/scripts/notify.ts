2f2b53b036ba48a28d08cfd26a6ed589260d246a
emmaramirez <ramirez.emma.g@gmail.com>
2016-06-27 17:53:38 -0700

    add historyOnly option

--- src/scripts/notify.ts
+++ src/scripts/notify.ts
@@ -5,24 +5,30 @@ interface note {
   time?:number;
   css?:string;
   history?:string[];
+  historyOnly?:boolean;
 }
 
 function notify(note:note):void {
   if (typeof note.time === 'undefined') {
     note.time = 2500;
   }
+  if (typeof note.historyOnly === 'undefined') {
+    note.historyOnly = false;
+  }
   // TODO: create settimeout
-  console.debug('Note was created with message of: ' + note.message);
-  let notification = document.createElement('div');
-  notification.className = 'notification';
 
-  notification.innerHTML = note.message;
-  notification.style.backgroundColor = note.color;
-  notification.setAttribute('style', note.css);
-  document.body.appendChild(notification);
-  setTimeout(function () {
-    notification.className = 'notification hidden';
-  }, note.time);
+  if (!note.historyOnly) {
+    console.debug('Note was created with message of: ' + note.message);
+    let notification = document.createElement('div');
+    notification.className = 'notification';
+    notification.innerHTML = note.message;
+    notification.style.backgroundColor = note.color;
+    notification.setAttribute('style', note.css);
+    document.body.appendChild(notification);
+    setTimeout(function () {
+      notification.className = 'notification hidden';
+    }, note.time);
+  }
 
   if (typeof note.history != 'undefined' && typeof note.year != 'undefined') {
     note.history.push(`<strong>${note.year}:</strong> note.message`);
