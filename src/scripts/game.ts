48b2b2cb6a6d5e7719d190ac6d35e8ae15528fd4
emmaramirez <ramirez.emma.g@gmail.com>
2016-06-30 18:17:19 -0700

    add totalLand property to Game class

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -19,6 +19,7 @@ class Game {
   era: Era;
   year: number;
   time: number;
+  totalLand: number;
   version: string;
 
   constructor(introStep: number) {
@@ -26,6 +27,7 @@ class Game {
     this.era = Era.Ancient;
     this.year = 0;
     this.time = 0;
+    this.totalLand = 14830000;
     this.version = '0.0.1';
   }
 }
