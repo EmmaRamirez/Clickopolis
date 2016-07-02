9d1339ac30a1806f534a761649cf3dae5eaad7e6
emmaramirez <ramirez.emma.g@gmail.com>
2016-07-01 18:43:30 -0700

    refactor: add <T> to wonders, clearer GP description

--- src/scripts/data.wonder.ts
+++ src/scripts/data.wonder.ts
@@ -1,8 +1,8 @@
 import Wonder = require('./wonder');
 import Collection = require('./collection');
 
-let greatPyramids = new Wonder('The Great Pyramids', 'great-pyramids', 10000, 10000, 'No aliens required.', 'This gives you a really neat bonus!');
+let greatPyramids = new Wonder('The Great Pyramids', 'great-pyramids', 10000, 10000, 'No aliens required.', '+25 <img src="img/legacy.png"> Points');
 
-let wonders:Collection = new Collection('wonders', [greatPyramids]);
+let wonders:Collection<Wonder> = new Collection('wonders', [greatPyramids]);
 
 export = wonders;
