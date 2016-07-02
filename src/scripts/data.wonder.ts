362e90412852bd6b32908e7d639aa591fac71453
emmaramirez <ramirez.emma.g@gmail.com>
2016-07-01 19:12:44 -0700

    style: wonders now prettier~

--- src/scripts/data.wonder.ts
+++ src/scripts/data.wonder.ts
@@ -1,8 +1,9 @@
 import Wonder = require('./wonder');
 import Collection = require('./collection');
 
-let greatPyramids = new Wonder('The Great Pyramids', 'great-pyramids', 10000, 10000, 'No aliens required.', '+25 <img src="img/legacy.png"> Points');
+let greatPyramids = new Wonder('The Great Pyramids', 'great-pyramids', 500, 500, 'No aliens required.', '+25 <img src="img/legacy.png"> Points');
+let stonehenge = new Wonder('Stonehenge', 'stonehenge', 700, 700, 'A glorified sundial...we think.', '+10 <img src="img/faith.png"> PM');
 
-let wonders:Collection<Wonder> = new Collection('wonders', [greatPyramids]);
+let wonders:Collection<Wonder> = new Collection('wonders', [greatPyramids, stonehenge]);
 
 export = wonders;
