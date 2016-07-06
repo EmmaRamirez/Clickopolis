424bb6ba64f871faa980c4169e27b84c101eeef4
emmaramirez <ramirez.emma.g@gmail.com>
2016-07-05 18:05:14 -0700

    add: 200 stones for testing

--- src/scripts/data.resource.ts
+++ src/scripts/data.resource.ts
@@ -3,7 +3,7 @@ import Collection = require('./collection');
 
 let food:Resource = new Resource('food', 1, 0, 1000, 0, 'food', 'Food.', true, true);
 let prod:Resource = new Resource('prod', 1, 0, 500, 0, 'prod', 'Prod.', true, true);
-let stone:Resource = new Resource('stone', 0, 0, -1, 0, 'stone', 'Stones are important as a building block for buildings.', false, true);
+let stone:Resource = new Resource('stone', 0, 0, -1, 200, 'stone', 'Stones are important as a building block for buildings.', false, true);
 let fish:Resource = new Resource('fish', 0, 0, -1, 0, 'fish', 'Fish are caught in nets by citizens periodically. Each fish provides +.5 <img src="img/health.png"> Fish are a popular trade item with Desert nations.', false, true);
 let banana:Resource = new Resource('banana', 0, 0, -1, 0, 'banana', 'Bananas are harvested by farmers periodically. Each banana provides +.5 <img src="img/health.png"> Bananas are a popular trade item with Tundra nations.', false, true);
 let spices:Resource = new Resource('spices', 0, 0, -1, 0, 'spices', 'Spices are a luxury resource your woodcutters provide for you. They are useful for religious civilizations.', false, true);
