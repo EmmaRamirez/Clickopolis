ee218fffa34619c9f10b10fbb29bcb9fdf7f206e
emmaramirez <ramirez.emma.g@gmail.com>
2016-07-13 18:17:23 -0700

    remove BiomeTypes

--- src/scripts/biome.ts
+++ src/scripts/biome.ts
@@ -1,17 +1,10 @@
-enum BiomeTypes {
-  Desert = <any>"Desert",
-  Jungle = <any>"Jungle",
-  Forest = <any>"Forest",
-  Island = <any>"Island",
-  Coast = <any>"Coast",
-  Mountain = <any>"Mountain",
-  Tundra = <any>"Tundra",
-  Glacier = <any>"Glacier"
-}
-
-interface Biome {
-  name: BiomeTypes;
+class Biome {
+  name: string;
   description: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
 }
 
 export = Biome;
