c96bc4ca51fea52bc733f2c1526cf5775f243066
emmaramirez <ramirez.emma.g@gmail.com>
2016-07-15 20:53:43 -0700

    add techUtils

---
+++ src/scripts/utils.tech.ts
@@ -0,0 +1,31 @@
+import Collection = require('./collection');
+import Tech = require('./tech');
+import techData = require('./data.tech');
+
+let techs = techData;
+
+
+class techUtils {
+
+
+
+  renderTechnologies(techSelector:HTMLElement, techs:Collection<Tech>):void {
+
+  }
+
+  unlockTech(tech:string):boolean {
+    if (!techs.get(tech).enabled) {
+      techs.get(tech).enabled = true;
+      return true;
+    } else {
+      throw new Error('Technology already enabled');
+    }
+  }
+
+  unlockTechs(techs:string):boolean {
+    return false;
+  }
+
+}
+
+export = techUtils;
