520b4af7a7a9512791e3402f451f6bcc33739927
Emma Ramirez <ramirez.emma.g@gmial.com>
2016-10-26 07:42:15 -0600

    notify when can't purchase faith bonus

--- src/scripts/utils.faith.ts
+++ src/scripts/utils.faith.ts
@@ -61,7 +61,7 @@ export function faithBonusClick(playerCiv) {
           updateFaithBonuses(playerCiv);
         }
       } else {
-        //notify({message: `You don't have enough faith to purchase this bonus!`});
+        notify({message: `You don't have enough faith to purchase this bonus!`}, true);
       }
     });
   });
