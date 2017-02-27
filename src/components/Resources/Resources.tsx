0755525c0b8e83d8ae3f9cccf67f03062f46bf39
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-02-27 12:57:04 -0700

    add Resources comp

---
+++ src/components/Resources/Resources.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Screen } from './Screen';
+
+export class Resources extends Screen<{}> {
+  public renderScreen() {
+    return (
+      <div>Resources.</div>
+    )
+  }
+}
