024c0e14442983b68aec8e18a80650c749c0748f
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-02-27 15:54:05 -0700

    add start section

---
+++ src/components/Start/Start.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export class Start extends React.Component<{}, {}> {
+  constructor(props) {
+    super(props);
+  }
+
+  public render():JSX.Element {
+    return (
+      <div className='start'>
+
+      </div>
+    )
+  }
+}
