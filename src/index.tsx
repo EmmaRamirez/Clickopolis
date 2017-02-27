7a9d3bbd754de693d4aee642e4e9cf0d48d63d92
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-02-27 12:57:42 -0700

    add component based architecture

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Root } from './components/Root';
+
+const mountNode = document.getElementById('app');
+
+ReactDOM.render(<Root />, mountNode);
