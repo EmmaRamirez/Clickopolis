761151edf18370f9055bf58c6b94689bae2e0ef8
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-03-08 03:22:26 -0700

    add new typings, use globals

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="custom/webpack.d.ts" />
+/// <reference path="globals/jest/index.d.ts" />
 /// <reference path="modules/react-dom/index.d.ts" />
 /// <reference path="modules/react/index.d.ts" />
-/// <reference path="custom/webpack.d.ts" />
