90fb49d4fe83e99e4edf24e66f0737fcc59dee5f
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-03-14 04:14:50 -0600

    try yet again to create a roundedCorners function

--- src/components/base/styles.ts
+++ src/components/base/styles.ts
@@ -2,7 +2,24 @@ import { StyleSheet, css } from 'aphrodite/no-important';
 
 type size = 'small' | 'medium' | 'large';
 
+interface Styles {
+  roundedCorners: (s: size) => object;
+}
 
-export const STYLES = {
+export const STYLES:Styles = {
+  roundedCorners: (size: size) => {
+    let s;
+    if (size === 'small') s = '.25rem';
+    if (size === 'medium') s = '.5rem';
+    if (size === 'large') s = '1rem';
+    return StyleSheet.create({
+      roundedCorners: {
+        borderRadius: s,
+      },
+    });
+  },
+
+
+
 
 };
